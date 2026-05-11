da10413091afe20a4af12bd03cef594ea8959876
VanesStalkers <ivan.mironov@bk.ru>
2026-05-11 14:23:43 +0400

    (-) removed examples

--- backend/application/api/example/getClientInfo.js
+++
@@ -1,9 +0,0 @@
-({
-  access: 'public',
-  method: async () => {
-    const { uuid, session, client } = context;
-    const { ip } = client;
-    const { token, userId } = session;
-    return { result: { ip, token, userId, uuid } };
-  },
-});
